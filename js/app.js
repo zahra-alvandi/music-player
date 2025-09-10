87870f8b0930818dbc2fefa471adb58286fd1e03
Zahra Alvandi <zalvandi39@gmail.com>
2025-09-10 11:17:41 +0330

    play and pause btn

--- js/app.js
+++ js/app.js
@@ -1,28 +1,78 @@
+// const music = document.querySelector("audio");
+// const tenSecForward = document.querySelector(".ten-sec-forward");
+// const tenSecBackward = document.querySelector(".ten-sec-back-forward");
+// const playBtn = document.querySelector(".play");
+// const playOrPauseBtn = document.querySelector("i");
+
+// // function tenSecForwardHandler() {
+// //     music.currentTime += 10;
+// // }
+// // function tenSecBackwardHandler() {
+// //     music.currentTime -= 10;
+// // }
+// // function playOrPauseBtnHandler() {
+// //     console.log("hi")
+// //     if (playOrPauseBtn.className.includes("fa-play")) {
+// //         music.play()
+// //         playOrPauseBtn.classList.remove("fa-play");
+// //         playOrPauseBtn.classList.add("fa-pause");
+// //     } else {
+// //         music.pause()
+// //         playOrPauseBtn.classList.remove("fa-pause")
+// //         playOrPauseBtn.classList.add("fa-play")
+// //     }
+// // }
+
+// // tenSecForward.addEventListener("click", tenSecForwardHandler);
+// // tenSecBackward.addEventListener("click", tenSecBackwardHandler);
+// // playOrPauseBtn.addEventListener("click", playOrPauseBtnHandler);
+
+// tenSecBackward.addEventListener("click", function () {
+//     music.currentTime += 10;
+// })
+
+// tenSecBackward.addEventListener("click", function () {
+//     music.currentTime -= 10;
+// })
+
+// playOrPauseBtn.addEventListener("click", function () {
+//     if (playOrPauseBtn.className.includes(".fa-play")) {
+//         music.play();
+//         playOrPauseBtn.classList.remove(".fa-play");
+//         playOrPauseBtn.classList.add(".fa-pause");
+//     } else {
+//         music.pause()
+//         playOrPauseBtn.classList.remove(".fa-pause");
+//         playOrPauseBtn.classList.add(".fa-pause");
+//     }
+// })
+
 const music = document.querySelector("audio");
 const tenSecForward = document.querySelector(".ten-sec-forward");
-const tenSecBackward = document.querySelector(".ten-sec-back-forward");
+const tenSecBackForward = document.querySelector(".ten-sec-back-forward");
 const playBtn = document.querySelector(".play");
-const playOrPauseBtn = document.querySelector("i");
+const playOrPauseBtn = playBtn.querySelector("i");
 
 function tenSecForwardHandler() {
-
+    music.currentTime += 10;
 }
-function tenSecBackwardHandler() {
 
+function tenSecBackForwardHandler() {
+    music.currentTime -= 10;
 }
-function playOrPauseBtnHandler() {
-    console.log("hi")
+
+function playOrPauseHandler() {
     if (playOrPauseBtn.className.includes("fa-play")) {
-        music.play()
+        music.play();
         playOrPauseBtn.classList.remove("fa-play");
         playOrPauseBtn.classList.add("fa-pause");
     } else {
-        music.pause()
-        playOrPauseBtn.classList.remove("fa-pause")
-        playOrPauseBtn.classList.add("fa-play")
+        music.pause();
+        playOrPauseBtn.classList.remove("fa-pause");
+        playOrPauseBtn.classList.add("fa-play");
     }
 }
 
 tenSecForward.addEventListener("click", tenSecForwardHandler);
-tenSecBackward.addEventListener("click", tenSecBackwardHandler);
-playOrPauseBtn.addEventListener("click", playOrPauseBtnHandler);
+tenSecBackForward.addEventListener("click", tenSecBackForwardHandler);
+playOrPauseBtn.addEventListener("click", playOrPauseHandler);
